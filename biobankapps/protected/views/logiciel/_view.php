51ac5c15cac1b0c96034e5b25814252ba13d983b
nicolas@malservet.eu <nicolas@malservet.eu>
2015-05-21 17:46:23 +0200

    logo and name clickable in th emain page to access to the view of software

--- biobankapps/protected/views/logiciel/_view.php
+++ biobankapps/protected/views/logiciel/_view.php
@@ -10,7 +10,7 @@
 	<br />
 
 	<b><?php echo CHtml::encode($data->getAttributeLabel('nom')); ?>:</b>
-	<?php echo CHtml::encode($data->nom); ?>
+	<?php echo CHtml::link($data->nom,array('logiciel/view')) ; ?>
 	<br />
 
 	<b><?php echo CHtml::encode($data->getAttributeLabel('societe')); ?>:</b>
@@ -32,84 +32,5 @@
 	<b><?php echo CHtml::encode($data->getAttributeLabel('prix')); ?>:</b>
 	<?php echo CHtml::encode($data->prix); ?>
 	<br />
-
-	<?php /*
-	<b><?php echo CHtml::encode($data->getAttributeLabel('descriptif_fr')); ?>:</b>
-	<?php echo CHtml::encode($data->descriptif_fr); ?>
-	<br />
-
-	<b><?php echo CHtml::encode($data->getAttributeLabel('descriptif_en')); ?>:</b>
-	<?php echo CHtml::encode($data->descriptif_en); ?>
-	<br />
-
-	<b><?php echo CHtml::encode($data->getAttributeLabel('screenshot_1')); ?>:</b>
-	<?php echo CHtml::encode($data->screenshot_1); ?>
-	<br />
-
-	<b><?php echo CHtml::encode($data->getAttributeLabel('screenshot_2')); ?>:</b>
-	<?php echo CHtml::encode($data->screenshot_2); ?>
-	<br />
-
-	<b><?php echo CHtml::encode($data->getAttributeLabel('screenshot_3')); ?>:</b>
-	<?php echo CHtml::encode($data->screenshot_3); ?>
-	<br />
-
-	<b><?php echo CHtml::encode($data->getAttributeLabel('screenshot_4')); ?>:</b>
-	<?php echo CHtml::encode($data->screenshot_4); ?>
-	<br />
-
-	<b><?php echo CHtml::encode($data->getAttributeLabel('screenshot_5')); ?>:</b>
-	<?php echo CHtml::encode($data->screenshot_5); ?>
-	<br />
-
-	<b><?php echo CHtml::encode($data->getAttributeLabel('logo_logiciel')); ?>:</b>
-	<?php echo CHtml::encode($data->logo_logiciel); ?>
-	<br />
-
-	<b><?php echo CHtml::encode($data->getAttributeLabel('keywords_fr')); ?>:</b>
-	<?php echo CHtml::encode($data->keywords_fr); ?>
-	<br />
-
-	<b><?php echo CHtml::encode($data->getAttributeLabel('keywords_en')); ?>:</b>
-	<?php echo CHtml::encode($data->keywords_en); ?>
-	<br />
-
-	<b><?php echo CHtml::encode($data->getAttributeLabel('contact_nom')); ?>:</b>
-	<?php echo CHtml::encode($data->contact_nom); ?>
-	<br />
-
-	<b><?php echo CHtml::encode($data->getAttributeLabel('contact_prenom')); ?>:</b>
-	<?php echo CHtml::encode($data->contact_prenom); ?>
-	<br />
-
-	<b><?php echo CHtml::encode($data->getAttributeLabel('contact_login')); ?>:</b>
-	<?php echo CHtml::encode($data->contact_login); ?>
-	<br />
-
-	<b><?php echo CHtml::encode($data->getAttributeLabel('contact_password')); ?>:</b>
-	<?php echo CHtml::encode($data->contact_password); ?>
-	<br />
-
-	<b><?php echo CHtml::encode($data->getAttributeLabel('contact_email')); ?>:</b>
-	<?php echo CHtml::encode($data->contact_email); ?>
-	<br />
-
-	<b><?php echo CHtml::encode($data->getAttributeLabel('contact_phone')); ?>:</b>
-	<?php echo CHtml::encode($data->contact_phone); ?>
-	<br />
-
-	<b><?php echo CHtml::encode($data->getAttributeLabel('langue_fr')); ?>:</b>
-	<?php echo CHtml::encode($data->langue_fr); ?>
-	<br />
-
-	<b><?php echo CHtml::encode($data->getAttributeLabel('langue_en')); ?>:</b>
-	<?php echo CHtml::encode($data->langue_en); ?>
-	<br />
-
-	<b><?php echo CHtml::encode($data->getAttributeLabel('langue_autres')); ?>:</b>
-	<?php echo CHtml::encode($data->langue_autres); ?>
-	<br />
-
-	*/ ?>
 
 </div>
