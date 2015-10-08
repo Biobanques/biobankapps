2faa2fa87b97c378f66c365da50b2f6468820522
nicolas <nicolas@macbook-pro>
2015-10-08 17:50:17 +0200

    fix bug if page more than 100% height

--- biobankapps/protected/views/layouts/main.php
+++ biobankapps/protected/views/layouts/main.php
@@ -17,34 +17,35 @@
 
         <!-- Bootstrap -->
         <link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->request->baseUrl; ?>/js/bootstrap-3.3.5-dist/css/bootstrap.min.css" />
-            <script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
+        <script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
 
-            <!-- custom js -->
-            <script src='js/script.js'></script>
-            <!-- bootstrap -->
-            <script src='js/bootstrap-3.3.5-dist/js/bootstrap.min.js'></script>
-            <!-- inclusion google analytics -->
-            <script type="text/javascript">
+        <!-- custom js -->
+        <script src='js/script.js'></script>
+        <!-- bootstrap -->
+        <script src='js/bootstrap-3.3.5-dist/js/bootstrap.min.js'></script>
+        <!-- inclusion google analytics -->
+        <script type="text/javascript">
 
-                var _gaq = _gaq || [];
-                _gaq.push(['_setAccount', 'UA-40385773-1']);
-                _gaq.push(['_trackPageview']);
+            var _gaq = _gaq || [];
+            _gaq.push(['_setAccount', 'UA-40385773-1']);
+            _gaq.push(['_trackPageview']);
 
-                (function () {
-                    var ga = document.createElement('script');
-                    ga.type = 'text/javascript';
-                    ga.async = true;
-                    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-                    var s = document.getElementsByTagName('script')[0];
-                    s.parentNode.insertBefore(ga, s);
-                })();
+            (function() {
+                var ga = document.createElement('script');
+                ga.type = 'text/javascript';
+                ga.async = true;
+                ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+                var s = document.getElementsByTagName('script')[0];
+                s.parentNode.insertBefore(ga, s);
+            })();
 
-            </script>
-            <title>biobankapps.com</title>
+        </script>
+        <title>biobankapps.com</title>
     </head>
     <body>
         <div class="container" id="page">
-            <?php
+            <div style="margin-top:0px;">
+                <?php
                 $this->widget('ext.bootstrap3.Navbar', array(
                     'items' => array(
                         array('label' => Yii::t('common', 'home'), 'url' => array('/site/index')),
@@ -54,12 +55,12 @@
                         array('label' => Yii::t('common', 'about'), 'url' => array('/site/page', 'view' => 'about')),
                         array('label' => Yii::t('common', 'login'), 'url' => array('/site/login'), 'visible' => Yii::app()->user->isGuest),
                         array('label' => Yii::t('common', 'logout') . ' (' . Yii::app()->user->name . ')', 'url' => array('/site/logout'), 'visible' => !Yii::app()->user->isGuest)
-                    
-                       ),
+                    ),
                 ));
                 ?>
-          
-            
+            </div>
+
+
             <!-- header -->
             <?php
             $flashMessages = Yii::app()->user->getFlashes();
@@ -70,34 +71,23 @@
             }
             ?>
             <?php echo $content; ?>
-
             <div class="clear"></div>
-
-            <div >
-                <div id="logo">
-                    
-                    
-                </div>
-            </div>
-             <div class="clear"></div>
-            <div id="footer">
-                
-            </div><!-- footer -->
-
-        </div><!-- page -->
-<nav class="navbar navbar-default navbar-fixed-bottom" role="navigation">
-  <div class="container">
-      <div style="float:left;">
+            <div style="height:60px;"/>
+            <nav class="navbar navbar-default navbar-fixed-bottom" role="navigation">
+                <div class="container">
+                    <div style="float:left;">
                         <?php echo CHtml::image(Yii::app()->request->baseUrl . '/images/logobb.png', 'logo', array('height' => 75, 'width' => 100)); ?>
                     </div>
-      <div style="float:left;padding-left:200px;">Version 1.1  Copyright &copy; <?php echo date('Y'); ?> by Biobanques.<br/>
-                Project Biobanques <a href="http://www.biobanques.eu">www.biobanques.eu</a><br/>
-                All Rights Reserved.<br/></div>
+                    <div style="float:left;padding-left:200px;">Version 1.1  Copyright &copy; <?php echo date('Y'); ?> by Biobanques.<br/>
+                        Project Biobanques <a href="http://www.biobanques.eu">www.biobanques.eu</a><br/>
+                        All Rights Reserved.<br/></div>
                     <div style="float:left;padding-left:200px;">
                         <?php echo CHtml::image(Yii::app()->request->baseUrl . '/images/bbmri_eric.png', 'logo', array('height' => 60, 'width' => 150)); ?>
                     </div>
-    
-  </div>
-</nav>
+
+                </div>
+            </nav>
+        </div><!-- page -->
+
     </body>
 </html>
