ec99304e4afd61624cc1d2b55b9b4da08dabe864
mpenicaud <matthieu.penicaud@inserm.fr>
2016-02-29 11:14:36 +0100

    add commonproperties file template

---
+++ components/CommonProperties.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace app\components;
+
+/*
+ * CommonProperties to store variables used for connexion etc.
+ * @author nicolas malservet
+ * @since version 0.16
+ */
+
+class CommonProperties
+{
+    /*
+     * DEV MODE : true or false.
+     * if true activate some refrences to the server to localhost and send mails to the from mail ( admin mail)
+     */
+    public static $DEV_MODE = true;
+    /*
+     * connection string used in ./protected/config/main_dev.php
+     */
+    public static $CONNECTION_STRING = 'mysql:host=yourhost;dbname=yourDbName';
+    public static $CONNECTION_LOGIN = 'login';
+    public static $CONNECTION_PASSWORD = 'pwd';
+    /**
+     * Mail system active: true if you want to send email.
+     */
+    public static $MAIL_SYSTEM_ACTIVE = true;
+    /*
+     * Admin email to send mails in case of errors or news.
+     */
+    public static $ADMIN_EMAIL = 'you@yourmail.com';
+    /*
+     * SMTP Sender. Allow the script sendmailcommand to send mails via smtp with autentication
+     */
+    public static $SMTP_SENDER_HOST = 'smtp_host';
+    public static $SMTP_SENDER_PORT = '25';
+    public static $SMTP_SENDER_USERNAME = 'SMTP_UN';
+    public static $SMTP_SENDER_PASSWORD = 'SMTP_PWD';
+    public static $SMTP_SENDER_FROM_EMAIL = 'appname@mail.com';
+}
