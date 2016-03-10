e2673558427fcdb5ed52df374c99e77e21a4eeaa
nicolas malservet <nicolas@malservet.eu>
2016-03-10 23:32:05 +0100

    select list into evaluation form for authors

--- views/evaluation/_form.php
+++ views/evaluation/_form.php
@@ -2,6 +2,8 @@
 
 use yii\helpers\Html;
 use yii\widgets\ActiveForm;
+use yii\helpers\ArrayHelper;
+use app\models\Author;
 
 /* @var $this yii\web\View */
 /* @var $model app\models\Evaluation */
@@ -12,13 +14,9 @@ use yii\widgets\ActiveForm;
 
     <?php $form = ActiveForm::begin(); ?>
 
-    <?= $form->field($model, 'id')->textInput() ?>
-
-    <?= $form->field($model, 'author_id')->textInput() ?>
+    <?= $form->field($model, 'author_id')->dropDownList(ArrayHelper::map(Author::find()->all(), 'id', 'name')) ?>
 
     <?= $form->field($model, 'date_evaluation')->textInput() ?>
-
-    <?= $form->field($model, 'grade')->textInput() ?>
 
     <div class="form-group">
         <?= Html::submitButton($model->isNewRecord ? 'Create' : 'Update', ['class' => $model->isNewRecord ? 'btn btn-success' : 'btn btn-primary']) ?>
