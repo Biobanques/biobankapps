c0e716f7dcbd8ff76fdc193e97f983bdc523b67c
nicolas malservet <nicolas@malservet.eu>
2016-06-10 09:09:02 +0200

    enable pretty url for api bibox start api controller add .hhtaccess to rewrite url internally

--- config/web.php
+++ config/web.php
@@ -12,8 +12,11 @@ $config = [
     'language' => 'en',
     'components' => [
         'request' => [
-            // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
+// !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
             'cookieValidationKey' => 'ON51dgbOPNH6541utGjG4562FTY4CZ5J',
+            'parsers' => [
+                'application/json' => 'yii\web\JsonParser',
+            ],
         ],
         'cache' => [
             'class' => 'yii\caching\FileCache',
@@ -28,58 +31,61 @@ $config = [
         'mailer' => [
             'class' => 'yii\swiftmailer\Mailer',
             // send all mails to a file by default. You have to set
-            // 'useFileTransport' to false and configure a transport
-            // for the mailer to send real emails.
+// 'useFileTransport' to false and configure a transport
+// for the mailer to send real emails.
             'useFileTransport' => true,
         ],
         'log' => [
-            //'traceLevel' => YII_DEBUG ? 3 : 0,
-            //traceLevel=3 enable file name and file number displaying into log 
-            'traceLevel'=>3,
+//'traceLevel' => YII_DEBUG ? 3 : 0,
+//traceLevel=3 enable file name and file number displaying into log 
+            'traceLevel' => 3,
             'flushInterval' => 1,
             'targets' => [
-               'file' => [
+                'file' => [
                     'class' => 'yii\log\FileTarget',
-                   'logFile' => '@runtime/logs/application.log',
+                    'logFile' => '@runtime/logs/application.log',
                 ],
-                /*'file' => [
-                    'class' => 'yii\log\FileTarget',
-                    'categories' => ['yii\web\HttpException:404'],
-                    'levels' => ['error', 'warning'],
-                    'logFile' => '@runtime/logs/404.log',
-                ],*/
+                /* 'file' => [
+                  'class' => 'yii\log\FileTarget',
+                  'categories' => ['yii\web\HttpException:404'],
+                  'levels' => ['error', 'warning'],
+                  'logFile' => '@runtime/logs/404.log',
+                  ], */
                 /*
                  * custom error log to only have errors in a straight way
                  */
                 'errorFile' => [
                     'class' => 'yii\log\FileTarget',
                     'levels' => ['error'],
-                    'logVars' => [],//no vars ( too verbose)
+                    'logVars' => [], //no vars ( too verbose)
                     'logFile' => '@runtime/logs/errors.log',
                 ],
-               /* 'email' => [
-                    'class' => 'yii\log\EmailTarget',
-                    'except' => ['yii\web\HttpException:404'],
-                    'levels' => ['error', 'warning'],
-                    'message' => ['from' => 'nicolas@malservet.eu', 'to' => 'nicolas@malservet.eu'],
-                ],*/
+            /* 'email' => [
+              'class' => 'yii\log\EmailTarget',
+              'except' => ['yii\web\HttpException:404'],
+              'levels' => ['error', 'warning'],
+              'message' => ['from' => 'nicolas@malservet.eu', 'to' => 'nicolas@malservet.eu'],
+              ], */
             ],
         ],
         'db' => require(__DIR__ . '/db.php'),
-        /*
-          'urlManager' => [
-          'enablePrettyUrl' => true,
-          'showScriptName' => false,
-          'rules' => [
-          ],
-          ],
-         */
+        'urlManager' => [
+            'enablePrettyUrl' => true,
+            'showScriptName' => false,
+            'enableStrictParsing' => false,
+            'rules' => array(
+                    '<controller:\w+>/<id:\d+>' => '<controller>/view',
+                '<controller:\w+>/<action:\w+>/<id:\d+>' => '<controller>/<action>',
+                '<controller:\w+>/<action:\w+>' => '<controller>/<action>',
+                 ),
+            
+        ],
         'i18n' => [
             'translations' => [
                 'common*' => [
                     'class' => 'yii\i18n\PhpMessageSource',
 //                    'basePath' => '@app/messages',
-                    //    'sourceLanguage' => 'en-US',
+//    'sourceLanguage' => 'en-US',
                     'fileMap' => [
                         'common' => 'common.php',
 //                    'app/error' => 'error.php',
@@ -96,10 +102,10 @@ $config = [
 
 if (YII_ENV_DEV) {
     // configuration adjustments for 'dev' environment
-   /*$config['bootstrap'][] = 'debug';
-    $config['modules']['debug'] = [
-        'class' => 'yii\debug\Module',
-    ];*/
+    /* $config['bootstrap'][] = 'debug';
+      $config['modules']['debug'] = [
+      'class' => 'yii\debug\Module',
+      ]; */
 
     $config['bootstrap'][] = 'gii';
     $config['modules']['gii'] = [
---
+++ controllers/ApiController.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace app\controllers;
+
+use yii\rest\ActiveController;
+
+class ApiController extends ActiveController
+{
+    public $modelClass = 'app\models\Software';
+}
