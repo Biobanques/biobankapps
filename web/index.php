de925027ad9fe7ebbee1c457dadc9aa225fe79b1
nicolas malservet <nicolas@malservet.eu>
2016-08-31 15:32:54 +0200

    add custom properties for circleci and switch on loading if necessary

--- web/index.php
+++ web/index.php
@@ -3,7 +3,12 @@
 // comment out the following two lines when deployed to production
 defined('YII_DEBUG') or define('YII_DEBUG', true);
 defined('YII_ENV') or define('YII_ENV', 'dev');
-include_once dirname(__FILE__) . '/../components/CommonProperties.php';
+if(getenv('CIRCLECI')=='true'){
+    include_once dirname(__FILE__) . '/../components/CommonPropertiesCircleCi.php';
+}else{
+    include_once dirname(__FILE__) . '/../components/CommonProperties.php';
+}
+
 include_once dirname(__FILE__) . '/../components/BBAConstants.php';
 require(__DIR__ . '/../vendor/autoload.php');
 require(__DIR__ . '/../vendor/yiisoft/yii2/Yii.php');
