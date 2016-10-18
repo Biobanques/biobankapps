50fc5ec44de9e7bbb1a335622dbb23502068258a
nicolas <nicolas@192.168.0.33>
2016-10-18 10:43:57 +0200

    add explanation on account needed

--- views/software/account_needed.php
+++ views/software/account_needed.php
@@ -1,8 +1,11 @@
 <?php
 use yii\helpers\Html;
 ?>
-<div class="alert alert-warning" role="alert">You need an account to add a software.<br>
+<div class="alert alert-warning" role="alert">You need an account to add a software.<br></div>
+<div>An account is free and allow you to do these followg tasks:
+    <ul><li>Publish softwares<li>Add reviews for softwares<li>Add quick and detailed analysis for softwares</ul></div>
     <div>
-        If you want to create an account, to publish a software, or review softwares, click on the button below :
+        If you want to create an account, click on the button below :
         <?= Html::a('Sign up', ['/site/signup'], ['class' => 'btn btn-primary']) ?>
-    </div></div>
+    </div>
+</div>
