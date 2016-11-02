528ed2906390404a3d610d1f0c031c98d54dbda9
nicolas <nicolas@192.168.0.33>
2016-11-02 10:45:54 +0100

    clean login page after css integration

--- views/site/login.php
+++ views/site/login.php
@@ -2,44 +2,37 @@
 /* @var $this yii\web\View */
 /* @var $form yii\bootstrap\ActiveForm */
 /* @var $model app\models\LoginForm */
+
 use yii\helpers\Html;
 use yii\bootstrap\ActiveForm;
 
 $this->title = 'Login';
 //$this->params['breadcrumbs'][] = $this->title;
 ?>
-<div class="site-login">
+<div class="page-header">
     <h1><?= Html::encode($this->title) ?></h1>
-
-    <p>Please fill out the following fields to login:</p>
-
-<?php
-$form = ActiveForm::begin([
-            'id' => 'login-form',
-            'options' => ['class' => 'form-horizontal'],
-            'fieldConfig' => [
-                'template' => "{label}\n<div class=\"col-lg-3\">{input}</div>\n<div class=\"col-lg-8\">{error}</div>",
-                'labelOptions' => ['class' => 'col-lg-1 control-label'],
-            ],
-        ]);
-?>
+    <div>
+        <p>Please fill out the following fields to login:</p>
+    </div>
+</div>
+<div class="row">
+    <div class="col-md-12">
+    <?php
+    $form = ActiveForm::begin([
+                'id' => 'login-form',
+    ]);
+    ?>
 
     <?= $form->field($model, 'username')->textInput(['autofocus' => true]) ?>
 
     <?= $form->field($model, 'password')->passwordInput() ?>
 
-    <?=
-    $form->field($model, 'rememberMe')->checkbox([
-        'template' => "<div class=\"col-lg-offset-1 col-lg-3\">{input} {label}</div>\n<div class=\"col-lg-8\">{error}</div>",
-    ])
-    ?>
+<?= $form->field($model, 'rememberMe')->checkbox() ?>
 
     <div class="form-group">
-        <div class="col-lg-offset-1 col-lg-11">
 <?= Html::submitButton('Login', ['class' => 'btn btn-primary', 'name' => 'login-button']) ?>
-        </div>
     </div>
 
 <?php ActiveForm::end(); ?>
-
+    </div>
 </div>
