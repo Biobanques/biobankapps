ea6f6dcd629732b0b2b7b443e1cfc8c59d9721e0
mpenicaud <matthieu.penicaud@inserm.fr>
2016-11-16 15:51:43 +0100

    update circle.yml

--- tests/unit/_bootstrap.php
+++ tests/unit/_bootstrap.php
@@ -5,6 +5,6 @@ require('components/CommonProperties.php');
 require('vendor/autoload.php');
 require('vendor/yiisoft/yii2/Yii.php');
 
-$config = require('config/tests.php');
+$config = require('config/web.php');
 
 (new yii\web\Application($config));
